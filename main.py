349663e415a1b91182692e44d18672db98310ad9
Sandi_Navoiy <santes.au@gmail.com>
2023-09-27 16:46:08 +0300

    изучение pandas

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+import pandas as pd
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    csv_filename = 'csv_data.csv'
+    df = pd.read_csv(csv_filename)
+    print(df.head(3)) #вывод перывых
+    print(df.tail(1)) #вывод последних
+    print(len(df)) #количество значений в таблице
+    print(df.groupby('price').count())
+    print(df.groupby('count').count())
+    print(df.groupby('add_cost').count())
+    print(df.groupby('product').count())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
