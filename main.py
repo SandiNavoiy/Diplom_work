6e936d51bac83ede3eb3188675e12d4d14cc8904
Sandi_Navoiy <santes.au@gmail.com>
2023-10-02 22:14:27 +0300

    Я не могу сделать тесты!

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from util.utils import WelcomeMessage, interact_with_user
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == "__main__":
     csv_filename = "csv_data.csv"
-    # df = pd.read_csv(csv_filename)
-    # print(df.head(3)) #вывод перывых
-    # print(df.tail(1)) #вывод последних
-    # print(len(df)) #количество значений в таблице
     print("Введите ваше имя")
     user_name = input()
     welcome_message = (
@@ -17,9 +13,3 @@ if __name__ == "__main__":
 
     # Запуск контекстного меню
     interact_with_user()
-
-    # print(df.groupby('price').count())
-    # print(df.groupby('count').count())
-    # print(df.groupby('add_cost').count())
-    # print(df.groupby('product').count())
-    #
